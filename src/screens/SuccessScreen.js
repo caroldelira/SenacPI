56809c38a8b59a74299b756d97cc0dee7259d2c4
Afonso Lima <afonsolima@gmail.com>
2024-02-25 13:45:35 -0300

    - icones e splash com logo do app - melhorias na responsividade das telas.

--- src/screens/SuccessScreen.js
+++ src/screens/SuccessScreen.js
@@ -1,74 +1,84 @@
-import React from 'react';
-import { View, StyleSheet, Text, Image, TouchableOpacity } from 'react-native';
+import React from "react";
+import { View, StyleSheet, Text, Image, TouchableOpacity } from "react-native";
 
 export function SuccessScreen({ navigation }) {
   return (
     <View style={styles.container}>
-      <View style={styles.logoContainer}>
-        <Image source={require('../../assets/successIcon.png')} style={styles.logo} />
-      </View>
+      <View style={styles.containerBody}>
+        <View style={styles.logoContainer}>
+          <Image
+            source={require("../../assets/successIcon.png")}
+            style={styles.logo}
+          />
+        </View>
 
-      <View style={styles.containerText}>
-        <Text style={styles.title}>Tudo pronto!</Text>
-        <Text style={styles.subtitle}>Comece já a</Text>
-        <Text style={styles.subtitle}>economizar!</Text>
+        <View style={styles.containerText}>
+          <Text style={styles.title}>Tudo pronto!</Text>
+          <Text style={styles.subtitle}>Comece já a</Text>
+          <Text style={styles.subtitle}>economizar!</Text>
+        </View>
       </View>
 
       <View style={styles.containerButton}>
         <TouchableOpacity
           style={styles.button}
-          onPress={() => navigation.navigate('Login')}
+          onPress={() => navigation.navigate("Login")}
         >
           <Text style={styles.buttonText}>Próximo</Text>
         </TouchableOpacity>
       </View>
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: "space-between",
+    height: "100%",
     padding: 20,
-    backgroundColor: '#F5F9FF',
+    backgroundColor: "#F5F9FF",
+  },
+  containerBody: {
+    flex: 1,
+    justifyContent: "space-evenly",
+    height: "100%",
   },
   logoContainer: {
-    alignItems: 'center',
-    marginBottom: 70,
-    marginTop: 100,
+    alignItems: "center",
+    marginBottom: 50,
   },
   logo: {
     width: 220,
     height: 220,
   },
   containerText: {
-    flexDirection: 'column',
-    justifyContent: 'flex-start',
-    textAlign: 'left',
-    marginBottom: 50,
+    flexDirection: "column",
+    justifyContent: "flex-start",
+    textAlign: "left",
   },
   title: {
     fontSize: 32,
-    fontWeight: '900',
-    color: '#191D88',
+    fontWeight: "900",
+    color: "#191D88",
   },
   subtitle: {
     fontSize: 32,
-    fontWeight: '900',
-    color: '#121824',
+    fontWeight: "900",
+    color: "#121824",
   },
   containerButton: {
-    alignItems: 'flex-end',
+    alignItems: "flex-end",
+    paddingVertical: 10,
   },
-    button: {
-    backgroundColor: '#191D88',
+  button: {
+    backgroundColor: "#191D88",
     paddingVertical: 16,
     borderRadius: 5,
-    width: '40%',
-    marginTop: 80,
+    width: "40%",
   },
   buttonText: {
-    color: '#FFFFFF',
-    textAlign: 'center',
+    color: "#FFFFFF",
+    textAlign: "center",
   },
 });
