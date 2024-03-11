3203215bf1f12ff37f70a64f8dd399342591d85c
Afonso Lima <afonsolima@gmail.com>
2024-03-10 22:55:29 -0300

    adicionada rotina de verificacao de update de versao

--- App.js
+++ App.js
@@ -1,27 +1,39 @@
-import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { StyleSheet } from 'react-native';
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { StyleSheet } from "react-native";
 
-import { SignUpScreen } from './src/screens/SignUpScreen';
-import { LoginScreen } from './src/screens/LoginScreen';
-import { SuccessScreen } from './src/screens/SuccessScreen';
-import { BottomTabNavigator } from './src/navigation/BottomTabNavigator';
-import { RecoverPassword } from './src/screens/RecoverPassword';
-import { CreateListScreen } from './src/screens/CreateListScreen';
-import { ProductsList } from './src/screens/ProductsList';
-import { CalculatorScreen } from './src/screens/CalculatorScreen';
-import { ReportScreen } from './src/screens/ReportScreen';
+import { SignUpScreen } from "./src/screens/SignUpScreen";
+import { LoginScreen } from "./src/screens/LoginScreen";
+import { SuccessScreen } from "./src/screens/SuccessScreen";
+import { BottomTabNavigator } from "./src/navigation/BottomTabNavigator";
+import { RecoverPassword } from "./src/screens/RecoverPassword";
+import { CreateListScreen } from "./src/screens/CreateListScreen";
+import { ProductsList } from "./src/screens/ProductsList";
+import { CalculatorScreen } from "./src/screens/CalculatorScreen";
+import { ReportScreen } from "./src/screens/ReportScreen";
+import * as Updates from "expo-updates";
+import { useEffect } from "react";
 
 const Stack = createStackNavigator();
 
 export default function App() {
+  useEffect(() => {
+    async function updateApp() {
+      const { isAvailable } = await Updates.checkForUpdateAsync();
+      if (isAvailable) {
+        await Updates.fetchUpdateAsync();
+        await Updates.reloadAsync();
+      }
+    }
+    updateApp();
+  }, []);
+
   return (
     <NavigationContainer>
-      
       <Stack.Navigator>
         <Stack.Screen
-          name='Login'
+          name="Login"
           component={LoginScreen}
           options={{
             headerShown: false,
@@ -80,14 +92,13 @@ export default function App() {
           }}
         />
 
-          <Stack.Screen
-            name="Navigation"
-            component={BottomTabNavigator}
-            options={{
-              headerShown: false,
-            }}
-          />
-
+        <Stack.Screen
+          name="Navigation"
+          component={BottomTabNavigator}
+          options={{
+            headerShown: false,
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -96,10 +107,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
-
-
--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -96,7 +96,7 @@ export function LoginScreen({ navigation }) {
           ) {
             // setUser("");
             // setPassword("");
-            navigation.navigate("Navigation");
+            navigation.navigate("Success");
           }
         })
         .finally(() => {});
